a24dad0b74269c13f50e43e870d35019f3394fad
vukhanhthyle <vukhanhthyle@usf.edu>
2022-01-28 16:51:56 -0500

    change add the submitted name

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+// Programmar: Vu Khanh Thy Le
+namespace Tech_Assignment_2___Basic_Console_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("If you born unique, don't died as a copy.");
+            Console.WriteLine("Submitted by Thy Le");
+        }
+    }
+}
